da394a6388fdc777dcabe8b2bd16249f52191564
Lakshan-Adhithyaa <slakshanadithyaa@gmail.com>
2025-02-04 21:58:02 +0530

    Update App.tsx

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Splash from './pages/Splash';
 import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import WorkoutCategories from './pages/WorkoutCategories';
 import Profile from './pages/Profile';
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Splash />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/user-info" element={<UserInfo />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/categories" element={<WorkoutCategories />} />
@@ -23,4 +25,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
